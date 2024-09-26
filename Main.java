eee26dc1dd002d3fb51ead4bc73e03aa4b82fdf5
astonHC <139020900+astonHC@users.noreply.github.com>
2024-09-26 17:57:34 +0100

    added blocks method rather than stack constructor

--- Main.java
+++ Main.java
@@ -11,6 +11,9 @@ public class Main
         Stack STACK = new Stack(100, 10);
 
        System.out.println("\n--- HARRY CLARK - JAVA MEMORY ALLOCATOR ---");
+
+       STACK.ADD_BLOCKS();
+
        System.out.println("\nAllocating Memory for the custom Stack:");
 
        int ADDRESS = STACK.ALLOC(0);
