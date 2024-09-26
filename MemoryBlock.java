b8366a622967001a738eb3b0a25b3c1db8d69333
astonHC <139020900+astonHC@users.noreply.github.com>
2024-09-26 17:53:38 +0100

    Update MemoryBlock.java

--- MemoryBlock.java
+++ MemoryBlock.java
@@ -4,18 +4,18 @@
 
 /* THIS FILE PERTAINS TOWARDS THE MEMORY BLOCK FUNCTIONALITY OF THE PROGRAM */
 
-public abstract class MemoryBlock implements Memory
+public class MemoryBlock extends BaseMemory
 {   
-    byte[] BLOCK_DATA;
-    int BLOCK_SIZE;
-    boolean ALLOCATED;
+    public byte[] BLOCK_DATA;
+    public int BLOCK_SIZE;
+    public boolean ALLOCATED;
 
     public MemoryBlock(){}
 
     public MemoryBlock(int SIZE)
     {
-        BLOCK_DATA = new byte[SIZE]; 
-        BLOCK_SIZE = SIZE;
-        ALLOCATED = false;
+        this.BLOCK_DATA = new byte[SIZE]; 
+        this.BLOCK_SIZE = SIZE;
+        this.ALLOCATED = false;
     }
 }
