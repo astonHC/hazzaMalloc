72722b992fca24fb73d4da12411e12761387485d
astonHC <139020900+astonHC@users.noreply.github.com>
2024-09-26 17:17:37 +0100

    removed interface

--- Memory.java
+++ Memory.java
@@ -10,35 +10,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public interface Memory
+public class BaseMemory
 {
-    public int ALLOC(int SIZE);
-    public void FREE(long VALUE);
-    public byte READ(int ADDRESS);
-    public void WRITE(int ADDRESS, byte VALUE);
-    public long GET_ADDRESS(int VALUE);
-
-    public byte[] DATA();
-    public boolean ALLOCATED();
-    public int SIZE();
-};
-
-abstract class BaseMemory implements Memory
-{
-    public List<MemoryBlock> BLOCKS;
+    public static List<MemoryBlock> BLOCKS;
     private static int BLOCK_INDEX;
     private static int OFFSET;
 
-    private static int TOP;
-    private static int BOTTOM;
-    private static int TOTAL_SIZE;
+    static int TOP;
+    static int BOTTOM;
+    static int TOTAL_SIZE;
+    static int BLOCK_SIZE;
+    static int BLOCK_NO = TOTAL_SIZE / BLOCK_SIZE;
 
-    private static final int BLOCK_NO = TOTAL_SIZE / BLOCK_INDEX;
+    public byte[] DATA;
+    public boolean ALLOCATED;
+    public int SIZE;
 
     public BaseMemory(){}
 
     public BaseMemory(int VALUE)
     {
+        super();
         this.BLOCKS = new ArrayList<>();
     }
     
@@ -47,11 +39,10 @@ abstract class BaseMemory implements Memory
 
     /* RETURN THE TYPE-CASTED DATA BASED ON THE PRE-REQ'S */
 
-    @Override
     public final long GET_ADDRESS(int VALUE)
     {
-        BLOCK_INDEX = VALUE / BLOCKS.get(0).SIZE();
-        OFFSET = VALUE % BLOCKS.get(0).SIZE();
+        BLOCK_INDEX = VALUE / BLOCKS.get(0).SIZE;
+        OFFSET = VALUE % BLOCKS.get(0).SIZE;
 
         return (long)BLOCKS.get(BLOCK_INDEX).BLOCK_DATA.length + OFFSET;
     }
@@ -59,11 +50,11 @@ abstract class BaseMemory implements Memory
     /* SAME METHOD EXCEPT THIS TIME, DATA IS EVALUATED ON THE BASIS OF AN ARRAY */
     /* WHICH STORES THE CURRENT OFFSET IN CHUNKS */ 
 
-    @Override
+
     public final byte READ(int ADDRESS)
     {
-        BLOCK_INDEX = ADDRESS / BLOCKS.get(0).SIZE();
-        OFFSET = ADDRESS % BLOCKS.get(0).SIZE();
+        BLOCK_INDEX = ADDRESS / BLOCKS.get(0).SIZE;
+        OFFSET = ADDRESS % BLOCKS.get(0).SIZE;
 
         return BLOCKS.get(BLOCK_INDEX).BLOCK_DATA[OFFSET];
     }
