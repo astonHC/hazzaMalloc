31d4a32be3aa160e672ed62cc9234c181f09f680
astonHC <139020900+astonHC@users.noreply.github.com>
2024-09-26 17:01:06 +0100

    added a whole bunch
    read comments for further context
    looking to modularise the functionality by accommodating for different constructor's

--- Stack.java
+++ Stack.java
@@ -2,7 +2,54 @@
 
 /* CUSTOM MEMORY ALLOCATOR FOR JAVA */
 
-public abstract class Stack extends BaseMemory
-{
+/* THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY OF THE STACK */
+/* UTILISING THE PRE-REQUISITES DEFINED IN THE BASE MEMORY CLASS */
 
+/* THIS AIMS TO PROVIDE A BASE, LIGHTWEIGHT SOLUTION TO ADDING AND FREE MEMORY */
+
+public abstract class Stack extends BaseMemory implements Memory
+{
+    public Stack(){}
+
+    /* A SUPER CONSTRUCTOR OBJECT BASED ON THE BASE MEMORY PRE-REQUISITE */
+
+    public Stack(int TOTAL_SIZE)
+    {
+        super();
+    }
+
+    /* HOWEVER, IF WE WANT TO ALLOCATE THE BLOCK SIZE ASYNCHRONOUSLY  */
+
+    public Stack(int TOTAL_SIZE, int BLOCK_SIZE)
+    {
+        this.TOTAL_SIZE = TOTAL_SIZE;
+        this.BLOCK_SIZE = BLOCK_SIZE;
+
+        ADD_BLOCKS();
+    }
+
+    /* ITERATE THROUGH EACH RESPECTIVE AND ALLOCATABLE AREA */
+    /* OF THE STACK BASED ON THE MEMORY BLOCK SIZE */
+
+    public static final void ADD_BLOCKS()
+    {
+        int INDEX = 0;
+        
+        for(INDEX = 0; INDEX < BLOCK_NO; INDEX++)
+        {
+            BLOCKS.add(new MemoryBlock(BLOCK_SIZE));
+        }
+
+        TOP -= 1;
+    }
+
+    /* ALLOCATE AND EVALUATE SPACE FOR EACH RESPECTIVE STACK */
+    /* ITERATE THROUGH EACH ELEMENT AND DETERMINE THE BLOCKS FROM TOP TO BOTTOM */
+
+    @Override
+    public int ALLOC(int SIZE)
+    {
+        
+        return TOP * BLOCKS.get(0).SIZE;
+    }
 }
